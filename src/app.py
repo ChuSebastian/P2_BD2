3a11e58a6c7793f6581124c2e92dce28ea6ff122
FlowersCS <carlos.flores.p@utec.edu.pe>
2024-06-14 04:02:35 -0500

    app con spotify api comentado

--- src/app.py
+++ src/app.py
@@ -3,32 +3,39 @@
 from flask import Flask, render_template, request
 import json
 import os
+#from spotify_api import get_spotify_token, get_track_image
 
 app = Flask(__name__, template_folder='../frontend/templates', static_folder='../frontend/static')
 
+# Configura tus credenciales de Spotify
+#CLIENT_ID = "3ea37211b94e4d00a7eb6c41c06d9a53"
+#CLIENT_SECRET = "b941ce9bee514798a8a41384ecd38bb5"
+#SPOTIFY_TOKEN = get_spotify_token(CLIENT_ID, CLIENT_SECRET)
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('base.html')
 # SIMULACION
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search_query():
-    lyrics = request.form['lyrics']
-    top_k = int(request.form['top_k'])
-    technique = request.form['technique']
+    if request.method == 'POST':
+        lyrics = request.form['lyrics']
+        top_k = int(request.form['top_k'])
+        technique = request.form['technique']
 
-    # Leer los resultados simulados del archivo JSON
-    json_path = os.path.join(os.path.dirname(__file__), '../data/results/outputEsperado.json')
-    with open(json_path, 'r') as f:
-        simulated_results = json.load(f)
+        # Simula resultados 
+        with open('data/results/outputEsperado.json', 'r') as file:
+            results = json.load(file)
 
-    # Filtramos los resultados según top_k
-    results = simulated_results[:top_k]
+        # Añade las imágenes a los resultados
+        #for result in results:
+        #    image_url = get_track_image(result['track_id'], SPOTIFY_TOKEN)
+        #    result['image'] = image_url
 
-    # Simulación de tiempo total (en segundos)
-    total_time = 0.1
-
-    return render_template('results.html', query=lyrics, technique=technique, results=results, total_time=total_time)
-
+        total_time = 25
+        return render_template('results.html', query=lyrics, technique=technique, results=results, total_time=total_time)
+    else:
+        return render_template('index.html', query=None, results=None)
 
 if __name__ == '__main__':
     app.run(debug=True)
